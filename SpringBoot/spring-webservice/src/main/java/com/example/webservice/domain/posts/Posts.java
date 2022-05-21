c8f1a992c67735d570789e5d8c812c4ca895190c
최유영 <choi62699919@gmail.com>
2022-05-22 02:27:41 +0900

    .

--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/domain/posts/Posts.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.webservice.domain.posts;
 
+import com.example.webservice.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
--- SpringBoot/spring-webservice/src/test/java/com/example/webservice/domain/posts/PostsRepositoryTest.java
+++ SpringBoot/spring-webservice/src/test/java/com/example/webservice/domain/posts/PostsRepositoryTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class PostsRepositoryTest {
@@ -37,5 +39,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("테스트 게시글"));
         assertThat(posts.getContent(), is("테스트 본문"));
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("choe46269909@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreateDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
