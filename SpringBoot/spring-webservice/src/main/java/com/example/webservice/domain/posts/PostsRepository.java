1fe2dfb0c84c3fd046ba597ffd71cacec7c84907
최유영 <choi62699919@gmail.com>
2022-05-22 01:00:02 +0900

    .

--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/domain/posts/PostsRepository.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/domain/posts/PostsRepository.java
@@ -1,4 +1,6 @@
 package com.example.webservice.domain.posts;
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
