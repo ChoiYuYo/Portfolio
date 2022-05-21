cc0198fa16df3fcfc197d992712651baf3a24576
최유영 <choi62699919@gmail.com>
2022-05-22 00:20:45 +0900

    최유영
    스프링부트

---
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/SpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebserviceApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/web/WebRestController.java
@@ -0,0 +1,4 @@
+package com.example.webservice.web;
+
+public class WebRestController {
+}
