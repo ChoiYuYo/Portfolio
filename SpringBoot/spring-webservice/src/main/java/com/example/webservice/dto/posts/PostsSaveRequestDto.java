d0cd4f5321d8fb7c867885fd0366caa0548bb0bd
최유영 <choi62699919@gmail.com>
2022-05-22 01:26:37 +0900

    .

---
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.webservice.dto.posts;
+
+import com.example.webservice.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/web/WebRestController.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/web/WebRestController.java
@@ -1,13 +1,25 @@
 package com.example.webservice.web;
 
+import com.example.webservice.domain.posts.PostsRepository;
+import com.example.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @GetMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
