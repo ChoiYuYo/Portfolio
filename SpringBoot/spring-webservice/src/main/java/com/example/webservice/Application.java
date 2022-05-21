65a7eeab0a0622bb6b6c9aa15def93d05f1e742e
최유영 <choi62699919@gmail.com>
2022-05-22 00:46:35 +0900

    .

--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/Application.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebserviceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/web/WebRestController.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/web/WebRestController.java
@@ -1,4 +1,13 @@
 package com.example.webservice.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
 }
