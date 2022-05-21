d867054b9cff9b321b706be0edffc23664da87cd
최유영 <choi62699919@gmail.com>
2022-05-22 05:08:03 +0900

    .

--- SpringBoot/spring-webservice/src/main/java/com/example/webservice/service/PostsService.java
+++ SpringBoot/spring-webservice/src/main/java/com/example/webservice/service/PostsService.java
@@ -1,11 +1,15 @@
 package com.example.webservice.service;
 
 import com.example.webservice.domain.posts.PostsRepository;
+import com.example.webservice.dto.posts.PostsMainResponseDto;
 import com.example.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @AllArgsConstructor
 @Service
@@ -15,5 +19,12 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
